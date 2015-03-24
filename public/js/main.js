690c44b841bf37a85aba68cda81da9a90360a4b2
Luke Lancaster <luketlancaster@gmail.com>
2015-03-24 11:51:27 -0500

    add files for stage two and refactor enemy spawning to be more precise

--- public/js/main.js
+++ public/js/main.js
@@ -21,7 +21,9 @@ var game = new Phaser.Game(800, 640, Phaser.AUTO, '', { preload: preload, create
     jumpSound,
     explosionSound,
     scream,
-    player;
+    player,
+    cupPath = [150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, -150, -150, -150, -150, -150, -150, -150, -150, -150, -150, -150, -150, -150, -150, -150, -150, -150, -150, -150, -150, -150, -150, -150, -150, -150, -150, -150, -150, -150, -150, -150, -150, -150, -150, -150, -150, -150, -150, -150],
+    cupIndex;
 
 function preload() {
   game.load.tilemap('background', './assets/groundfloor.json', null, Phaser.Tilemap.TILED_JSON);
@@ -43,6 +45,7 @@ function preload() {
 function create() {
   game.physics.startSystem(Phaser.Physics.ARCADE);
   fireKey = game.input.keyboard.addKey(Phaser.Keyboard.A);
+  cupIndex = 0;
 
   //sounds
   jumpSound = game.add.audio('jump');
@@ -83,39 +86,64 @@ function create() {
 
   cups = game.add.group();
   cups.enableBody = true;
-  for(var i = 0; i < 20; i++) {
-    cup = cups.create(i * 420, 200, 'cup');
+  cups.createMultiple(20, 'cup');
+  cups.setAll('collideWorldBounds', true);
+  cups.setAll('body.gravity.y', 950);
+  cups.forEach(function(cup) {
     game.physics.enable(cup, Phaser.Physics.ARCADE);
-    cup.body.collideWorldBounds = true;
+    cup.anchor.set(0.5, 0.5);
     cup.body.setSize(52, 63);
     cup.body.gravity.y = 950;
     cup.animations.add('left', [0, 1, 2, 3], 10, true);
     cup.animations.add('right', [0, 3, 2, 1], 10, true);
-  }
+  });
 
+  var cupPosition = [];
+  var counter = 0;
+
+  cups.forEach(function(cup) {
+    cup.reset(cupPosition[counter], 400);
+    counter++;
+  }, this);
+
+  // for(var i = 0; i < 20; i++) {
+  //   cup = cups.create(i * 420, 200, 'cup');
+  //   game.physics.enable(cup, Phaser.Physics.ARCADE);
+  //   cup.body.collideWorldBounds = true;
+  //   cup.body.setSize(52, 63);
+  //   cup.body.gravity.y = 950;
+  //   cup.animations.add('left', [0, 1, 2, 3], 10, true);
+  //   cup.animations.add('right', [0, 3, 2, 1], 10, true);
+  // }
+
+
+  //cans
   coffeecans = game.add.group();
   coffeecans.enableBody = true;
-  for(var i = 0; i < 20; i++) {
-    coffeecan = coffeecans.create((i * 1150), 350, 'coffeecan');
-    coffeecan.body.collideWorldBounds = true;
-    //coffeecan.body.setSize(41, 48);
-    coffeecan.body.gravity.y = 350;
-  }
+  coffeecans.createMultiple(20, 'coffeecan');
+  coffeecans.setAll('collideWorldBounds', true);
+  coffeecans.forEach(function(coffeecan) {
+    game.physics.enable(coffeecan, Phaser.Physics.ARCADE);
+    coffeecan.body.gravity.y = 950;
+    coffeecan.anchor.set(0.5, 0.5);
+    coffeecan.body.setSize(41, 48);
+  });
+
+  var coffeecanPosition = [220, 440];
+  var counter = 0;
+
+  coffeecans.forEach(function(coffeecan) {
+    coffeecan.reset(coffeecanPosition[counter], 400);
+    counter++;
+  }, this);
+
 
   //bowties
   bowties = game.add.group();
   bowties.enableBody = true;
-  bowties.physicsBodyType = Phaser.Physics.ARCADE;
-  for(var i = 0; i < 20; i++) {
-    var b = bowties.create(100000, 1000000, 'bowtie');
-    b.name = 'bowtie' + i;
-    b.body.gravity.y = 0;
-    b.body.setSize(64, 36);
-    b.exists = false;
-    b.visible = true;
-    b.events.onOutOfBounds.add(resetBowtie, this);
-    b.checkWorldBounds = true;
-  }
+  bowties.createMultiple(2, 'bowtie');
+  bowties.setAll('body.setSize', 64, 36);
+  //bowties.physicsBodyType = Phaser.Physics.ARCADE;
 
   game.cameraLastX = game.camera.x;
   game.cameraLastY = game.camera.y;
@@ -171,32 +199,23 @@ function update() {
     }
 
     //cups
-    for(var i = 0; i < 20; i++) {
-      cups.children[0].kill();
-      cups.children[19].kill();
-      if(cups.children[i].body.onFloor()) {
-        cups.children[i].body.velocity.y = -350;
-        cups.children[i].animations.play('left');
+    cups.forEachAlive(function(cup) {
+      if(cup.body.onFloor()) {
+        cup.body.velocity.y = -350;
+        cup.animations.play('left');
       }
-    }
+    });
 
     //coffeecans
-    // coffeecans.forEachAlive(function(coffeecan){
-    //   coffeecan.body.velocity.x = -300;
-    //   if(coffeecan.body.touching.left || coffeecan.body.touching.right) {
-    //     coffeecan.body.velocity.x *= -1;
-    //   }
-    // });
-
-    // for(var u = 0; u < 20; u++) {
-    //   if(coffeecans.children[u].body.onFloor) {
-    //     coffeecans.children[u].body.velocity.x = 300;
-    //   }
-    //   if(coffeecans.children[u].body.blocked.right) {
-    //     coffeecans.children[u].body.velocity.x *= -1;
-    //     //coffeecans.children[u].body.velocity.y = 2000;
-    //   }
-    // }
+    // console.log('cupIndex', cupIndex);
+    // console.log('cupPath.length', cupPath.length);
+    // console.log('cupIndex === 0', cupIndex === 0);
+    // console.log('cupIndex === cupPath.length - 1', cupIndex === cupPath.length - 1);
+    coffeecans.forEachAlive(function(can){
+      can.body.velocity.x = 0;
+      can.body.velocity.x = cupPath[cupIndex];
+    });
+    cupIndex = cupIndex + 1 >= cupPath.length ? 0 : cupIndex + 1;
 
     //bowties
     bowties.forEachAlive(function(bowtie){
